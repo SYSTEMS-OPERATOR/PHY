c56ab7b78e94cfecfa63ef8a00de1b092e0093b3
SYSTEMS-OPERATOR <155610697+SYSTEMS-OPERATOR@users.noreply.github.com>
2025-06-23 00:04:59 -0400

    Add bionic entanglement features

--- assemble_skeleton.py
+++ assemble_skeleton.py
@@ -1,7 +1,9 @@
 from skeleton import load_bones
+from skeleton.field import SkeletonField
 import json
 
 if __name__ == "__main__":
     bones = load_bones()
-    data = {b.unique_id: b.__dict__ for b in bones}
+    field = SkeletonField(bones)
+    data = {b.unique_id: b.self_state() for b in bones}
     print(json.dumps(data, indent=2))
